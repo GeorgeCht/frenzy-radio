1740d2ad3b1aba7d46c4724503eeb55616eec934
George Chatziiordanou <panos.anastasiadis@outlook.com>
2023-04-24 14:35:29 +0300

    Updated .gitignore

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.frenzyradio.app',
+  appName: 'Frenzy Radio',
+  webDir: 'out',
+  bundledWebRuntime: false
+};
+
+export default config;
